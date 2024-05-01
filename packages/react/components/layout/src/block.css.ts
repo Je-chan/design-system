941f253804dc226903234518b185a7ab936fdec6
ye chan <jkyyc3@gmail.com>
2024-05-02 00:29:49 +0900

    feat: layout 컴포넌트

---
+++ packages/react/components/layout/src/block.css.ts
@@ -0,0 +1,7 @@
+import { vars } from "@mg/themes";
+import { style } from "@vanilla-extract/css";
+export const blockStyle = style({
+  width: "100px",
+  height: "100px",
+  backgroundColor: vars.colors.$scale.blue[500],
+});
